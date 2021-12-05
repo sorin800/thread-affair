b1568ed4f475a87b51bce28d7c09a2080b134bdb
Sorin Truica <sorin.truica@gmail.com>
2021-12-05 14:45:47 +0200

    tema

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import './../scss/index.scss';
+
+import Glide from '@glidejs/glide';
+
+new Glide('.home-banner.glide', {
+  type: 'carousel',
+}).mount();
+
+new Glide('.product-tile-list > .glide', {
+  type: 'carousel',
+}).mount();
